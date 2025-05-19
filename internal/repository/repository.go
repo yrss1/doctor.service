2d8249084985883ccb79746159feb50e61efacd2
Yerassyl Rymkul <yrssl@Yerassyls-MacBook-Pro.local>
2025-05-19 14:11:54 +0500

    fix

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -43,9 +43,9 @@ func WithPostgresStore(dbName string) Configuration {
 		if err != nil {
 			return fmt.Errorf("failed to initialize database connection: %w", err)
 		}
-		if err = store.Migrate(dbName); err != nil {
-			return fmt.Errorf("failed to run database migrations: %w", err)
-		}
+		// if err = store.Migrate(dbName); err != nil {
+		// 	return fmt.Errorf("failed to run database migrations: %w", err)
+		// }
 
 		r.Doctor = postgres.NewDoctorRepository(r.postgres.Client)
 		r.Clinic = postgres.NewClinicRepository(r.postgres.Client)
